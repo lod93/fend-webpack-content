40255ade3c2f86e878e035b1cc15ff6a452f2db3
lod93 <problematicdelegate@gmail.com>
2021-04-29 18:30:46 +0200

    changes

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,9 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
-
+import "./styles/base.css"
+import "./styles/header.css"
+import "./styles/form.css"
+import "./styles/resets.css"
 console.log(checkForName);
 
 alert("I EXIST")
--- webpack.prod.js
+++ webpack.prod.js
@@ -11,7 +11,11 @@ module.exports = {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
-            }
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+              },
         ]
     },
     plugins: [
